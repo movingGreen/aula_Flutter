7bb1dc6c12092ed00af61c625c638e8145cdb0b4
Elizeu <el_vailant@hotmail.com>
2023-11-12 21:33:47 -0400

    tentando montar geolocalizacao

--- lib/pages/rastreamento/rastreamento_page.dart
+++ lib/pages/rastreamento/rastreamento_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:prog_app/services/users/users_services.dart';
+import 'package:provider/provider.dart';
+import '';
 
 class RastreamentoPage extends StatefulWidget {
   const RastreamentoPage({super.key});
@@ -11,32 +13,35 @@ class RastreamentoPage extends StatefulWidget {
 }
 
 class _RastreamentoPageState extends State<RastreamentoPage> {
-
-  Future position = await _;
+  Future _positionUser = determinePosition();
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        FlutterMap(
-          options: const MapOptions(
-            initialCenter: LatLng(51.509364, -0.128928),
-            initialZoom: 3.2,
-          ),
-          children: [
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'com.example.app',
+    return Consumer<UsersServices>(builder: (context, userServices, _) {
+      return Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              initialCenter: userServices.determinePosition().then((position) => {
+                new LatLng(position.latitude, position.longitude);
+              }),
+              initialZoom: 3.2,
             ),
-            RichAttributionWidget(attributions: [
-              TextSourceAttribution(
-                'OpenStreetMap contributors',
-                onTap: () => {},
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
               ),
-            ])
-          ],
-        ),
-      ],
-    );
+              RichAttributionWidget(attributions: [
+                TextSourceAttribution(
+                  'OpenStreetMap contributors',
+                  onTap: () => {},
+                ),
+              ])
+            ],
+          ),
+        ],
+      );
+    });
   }
 }
--- lib/services/users/users_services.dart
+++ lib/services/users/users_services.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:prog_app/models/users/users.dart';
+import 'package:geolocator/geolocator.dart';
 
 class UsersServices extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -13,6 +14,33 @@ class UsersServices extends ChangeNotifier {
   }
 
   DocumentReference get _firetoreRef => _firestore.doc('users/${users.id}');
+
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
 
   //Método para registrar o usuário no firebase console
   Future<bool> signUp(
